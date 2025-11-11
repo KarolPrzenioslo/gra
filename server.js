4f10d2614630f5a0a6db2a74d05ef7516b36ba8c
Karol Przeniosło <karol.p0621@gmail.com>
2025-11-11 19:00:06 +0100

    pierwszy commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "*" }
+});
+
+app.get("/", (req, res) => res.send("Serwer działa ✅"));
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server działa na porcie ${PORT}`));
