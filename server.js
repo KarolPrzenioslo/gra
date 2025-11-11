fd8788ae58decd297a022abfef5dcd681d2aa955
Karol Przeniosło <karol.p0621@gmail.com>
2025-11-11 19:27:07 +0100

    Dodano frontend i serwowanie plików

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const path = require("path");
-app.use(express.static(path.join(__dirname)));
-
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: { origin: "*" }
-});
+const io = new Server(server, { cors: { origin: "*" } });
 
+// serwowanie frontendu
+app.use(express.static(path.join(__dirname)));
+
+// Socket.IO
+io.on("connection", (socket) => {
+    console.log("Nowy gracz:", socket.id);
+    socket.on("join", (nick) => {
+        console.log(nick, "dołączył");
+    });
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server działa na porcie ${PORT}`));
